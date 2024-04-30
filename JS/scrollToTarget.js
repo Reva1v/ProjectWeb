da3d60dcc4e6c436040e526b2751f08449d41524
Reva1v <omruga06@gmail.com>
2024-04-30 17:02:30 +0300

    Fix scrollToTarget.js

--- JS/scrollToTarget.js
+++ JS/scrollToTarget.js
@@ -1,8 +1,8 @@
 function scrollToForProjects() {
-    let targetElement = document.querySelector('.for_projects');
+    let targetElement = document.querySelector('.for_projects__content');
     targetElement.scrollIntoView({behavior: 'smooth'});
 }
 function scrollToForInvestors() {
-    let targetElement = document.querySelector('.for_investors');
+    let targetElement = document.querySelector('.for_investors__container');
     targetElement.scrollIntoView({behavior: 'smooth'});
 }
